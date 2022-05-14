a67bb28fa9da64b8364bfd02431b2d53a6593f19
mopholosiCodes@gmail.com <mopholosiCodes@gmail.com>
2022-05-14 06:48:11 +0200

    Version2.0

---
+++ BusinessLayer/GenerateValues.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLayer;
+
+namespace BusinessLayer
+{
+    public class GenerateValues
+    {
+        public Storage storage = new Storage();
+
+        // generate age not working
+        public int GenerateAge(long idNumber)
+        {
+            var date = new DateTime();
+            var Id = idNumber.ToString().Substring(0, 1);
+
+            return date.Year - Convert.ToInt32(Id);
+        }
+
+        // email
+        public string GenerateEmail(string fname, string lname)
+        {
+            var name = fname.Substring(0).ToLower();
+            var surname = lname.ToLower();
+
+            return $"{name}{surname}@uj.ac.za";
+        }
+
+        public void DisplayData()
+        {
+            var list = storage.ReadData();
+            var StaffInfo = from element in list
+                            orderby element.Age
+                            select element;
+
+            foreach(var element in StaffInfo)
+                Console.WriteLine($"{element.FirstName} {element.LastName} {element.Email} {element.Age}");
+        }
+    }
+}
---
+++ DataLayer/Storage.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DataLayer
+{
+    public class Storage
+    {
+        private string filePath = "StaffMembers.txt";
+        public List<StaffMember> staffMembers = new List<StaffMember>();
+
+        // Create
+        public void StoreData(string fname, string lname, string email, int age)
+        {
+            using (StreamWriter sw = new StreamWriter(filePath,true))
+            {
+                sw.WriteLine($"{fname},{lname},{email},{age}");
+                sw.Close();
+            }
+        }
+
+        // Read
+        public List<StaffMember> ReadData()
+        {
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var data = sr.ReadLine().Split(',');
+                    staffMembers.Add(new StaffMember
+                    {
+                        FirstName = data[0],
+                        LastName = data[1],
+                        Email = data[2],
+                        Age = Convert.ToInt32(data[3])
+                    });
+                }
+                sr.Close();
+            }
+            return staffMembers;
+        }
+    }
+}
